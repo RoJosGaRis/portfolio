6906737555a55721e0da30e8a4f6948383d7976a
RoJosGaRis <roberto.jgr03@gmail.com>
2023-10-07 21:49:16 -0600

    continuing changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { useState, useRef, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
+import reactLogo from "./components/react.svg";
 import viteLogo from "/vite.svg";
 import styles from "./App.module.css";
 import anime from "animejs/lib/anime.es.js";
 
-import Airplane from "./assets/Airplane/airplane";
-import Hero from "./assets/Hero/Hero";
-import ContainerCard from "./assets/ContainerCard/ContainerCard";
+import Airplane from "./components/Airplane/airplane";
+import Hero from "./components/Hero/Hero";
 
 const App = () => {
   return (
